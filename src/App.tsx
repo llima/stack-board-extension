2ce46d9956ec0ffbd625b33d7c2e98d4dc927962
Luiz Lima <luiz.lima@elevenfinancial.com>
2021-07-06 17:01:00 -0300

    add: react initial structure

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Stack board
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
